0894601d1431adf5638c9dfb9cd13b05764e94da
Kristian Erendi <kristianerendi@erendi.local>
2021-02-05 15:29:37 +0100

    cleanup

--- src/settingsFile.js
+++ src/settingsFile.js
@@ -1,20 +1,40 @@
-import {Link} from "react-router-dom";
-import {ReactComponent as PinSVG} from "./design-tokens/icons/pin.svg";
+import { Link } from "react-router-dom";
+import { ReactComponent as PinSVG } from "./design-tokens/icons/pin.svg";
 import React from "react";
 
 export const settingsFile = [
-    {id: 'showOnlyDaylight', text: 'Show only daylight hours', type: 'checkbox',  value: true},
-    {id: 'showKitesurf', text: 'Show kite surfing spots', type: 'checkbox',  value: true},
-    {id: 'showSnowkite', text: 'Show snow kite spots', type: 'checkbox',  value: false},
-    {id: 'showSpecial', text: 'Show only special spots', type: 'checkbox',  value: false},
+  {
+    id: "showOnlyDaylight",
+    text: "Show only daylight hours",
+    type: "checkbox",
+    value: true,
+  },
+  {
+    id: "showKitesurf",
+    text: "Show kite surfing spots",
+    type: "checkbox",
+    value: true,
+  },
+  {
+    id: "showSnowkite",
+    text: "Show snow kite spots",
+    type: "checkbox",
+    value: false,
+  },
+  {
+    id: "showSpecial",
+    text: "Show only special spots",
+    type: "checkbox",
+    value: false,
+  },
 ];
 
 export const getPath = {
-    'home': '/',
-    'summary': '/summary',
-    'settings': '/settings',
-    'detail': '/detail',
-    'debug': '/debug',
-    'img': '/img/icon/',
-    'weatherIcon': '/img/weathericon/svg/',
+  home: "/",
+  summary: "/summary",
+  settings: "/settings",
+  detail: "/detail",
+  debug: "/debug",
+  img: "/img/icon/",
+  weatherIcon: "/img/weathericon/svg/",
 };
