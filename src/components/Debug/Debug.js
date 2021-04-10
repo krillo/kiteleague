b3afb77ae4006d8ccbbf55482eb5f6beb1dd1956
Kristian Erendi <krillo@gmail.com>
2021-04-10 09:02:30 +0200

    try out material UI

--- src/components/Debug/Debug.js
+++ src/components/Debug/Debug.js
@@ -3,35 +3,44 @@ import './Debug.scss';
 import Spinner from '../Spinner/Spinner';
 import SpotDirection from "../SpotDirection/SpotDirection";
 import {Route} from "react-router-dom";
+import {Button} from '@material-ui/core';
 
-class Debug extends Component {
-    constructor(props) {
-        super(props);
-        this.state =  {
-        }
+
+// interface IProps {
+//     personId: string
+// }
+
+// const Debug = ({ personId }: IProps) => {
+const Debug = () => {
+
+    const Butt = () => {
+        return <Button color="primary">Hello World</Button>
     }
 
-    render() {
-        return (
-            <div>
-                <Spinner />
-                <div className={'debug-spot-direction'}>
-                    <SpotDirection dirMin={270} dirMax={20} wind={10}  badColor={'#e5e5e5'}/>
-                    <SpotDirection dirMin={90} dirMax={45} wind={10}  badColor={'#e5e5e5'}/>
-                    <SpotDirection dirMin={200} dirMax={300} wind={10}  badColor={'#e5e5e5'}/>
-                    <SpotDirection dirMin={0} dirMax={90} wind={10}  badColor={'#e5e5e5'}/>
-                    <SpotDirection dirMin={90} dirMax={270} wind={10}  badColor={'#e5e5e5'}/>
-                    <SpotDirection dirMin={90} dirMax={300} wind={10}  badColor={'#e5e5e5'}/>
-                    <SpotDirection dirMin={0} dirMax={360} wind={10}  badColor={'#e5e5e5'}/>
-                    <SpotDirection dirMin={0} dirMax={0} wind={10}  badColor={'#e5e5e5'}/>
-                    <SpotDirection dirMin={300} dirMax={360} wind={10}  badColor={'#e5e5e5'}/>
-                    <SpotDirection dirMin={10} dirMax={45} wind={10}  badColor={'#e5e5e5'}/>
-                    <SpotDirection dirMin={300} dirMax={200} wind={10}  badColor={'#e5e5e5'}/>
-                    <SpotDirection dirMin={180} dirMax={290} wind={10}  badColor={'#e5e5e5'}/>
-                </div>
+    return (
+        <>
+            <Spinner/>
+            <div className={'debug-spot-direction'}>
+                <SpotDirection dirMin={270} dirMax={20} wind={10} badColor={'#e5e5e5'}/>
+                <SpotDirection dirMin={90} dirMax={45} wind={10} badColor={'#e5e5e5'}/>
+                <SpotDirection dirMin={200} dirMax={300} wind={10} badColor={'#e5e5e5'}/>
+                <SpotDirection dirMin={0} dirMax={90} wind={10} badColor={'#e5e5e5'}/>
+                <SpotDirection dirMin={90} dirMax={270} wind={10} badColor={'#e5e5e5'}/>
+                <SpotDirection dirMin={90} dirMax={300} wind={10} badColor={'#e5e5e5'}/>
+                <SpotDirection dirMin={0} dirMax={360} wind={10} badColor={'#e5e5e5'}/>
+                <SpotDirection dirMin={0} dirMax={0} wind={10} badColor={'#e5e5e5'}/>
+                <SpotDirection dirMin={300} dirMax={360} wind={10} badColor={'#e5e5e5'}/>
+                <SpotDirection dirMin={10} dirMax={45} wind={10} badColor={'#e5e5e5'}/>
+                <SpotDirection dirMin={300} dirMax={200} wind={10} badColor={'#e5e5e5'}/>
+                <SpotDirection dirMin={180} dirMax={290} wind={10} badColor={'#e5e5e5'}/>
             </div>
-        )
-    }
+            <Butt/>
+        </>
+
+
+    )
 }
 
-export default Debug;
+export default Debug
+
+
